53eec5b4632c88e6b0cf63598762f813f1767c56
austhomi <austhomi.k@hotmail.com>
2023-06-01 10:42:10 +0800

    change to use Hostbinding to change the style of a element

--- shopping-cart/src/app/directives/better-highlighter.directive.ts
+++ shopping-cart/src/app/directives/better-highlighter.directive.ts
@@ -1,10 +1,10 @@
-import { Directive, ElementRef, Renderer2, OnInit, HostListener} from '@angular/core';
+import { Directive, ElementRef, Renderer2, OnInit, HostListener, HostBinding} from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlighter]'
 })
 export class BetterHighlighterDirective implements OnInit{
-
+  @HostBinding('style.backgroundColor') backgroundColor: string;
   constructor(private elRef:ElementRef, private renderer: Renderer2) { }
 
   ngOnInit(){
@@ -12,12 +12,14 @@ export class BetterHighlighterDirective implements OnInit{
   }
 
   @HostListener('mouseenter') mouseover(eventData: Event){
-    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue')
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue')
+    this.backgroundColor = 'blue';
     this.renderer.setStyle(this.elRef.nativeElement, 'color', 'white')
   }
 
   @HostListener('mouseleave') mouseleave(eventData: Event){
-    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent')
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent')
+    this.backgroundColor = 'white';
     this.renderer.setStyle(this.elRef.nativeElement, 'color', 'black')
   }
 
