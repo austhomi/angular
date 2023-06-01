2dc820790eb43c9a4da6ce54d898ba8ec1b42085
austhomi <austhomi.k@hotmail.com>
2023-06-01 15:59:59 +0800

    service practice

--- service-start/src/app/app.component.ts
+++ service-start/src/app/app.component.ts
@@ -1,31 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, OnInit } from '@angular/core';
+import { AccountService } from './accounts.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [AccountService]
 })
-export class AppComponent {
-  accounts = [
-    {
-      name: 'Master Account',
-      status: 'active'
-    },
-    {
-      name: 'Testaccount',
-      status: 'inactive'
-    },
-    {
-      name: 'Hidden Account',
-      status: 'unknown'
-    }
-  ];
+export class AppComponent implements OnInit, OnChanges {
+  accounts : {name:string, status:string}[] = [];
 
+  constructor(private accountService: AccountService) {}
   onAccountAdded(newAccount: {name: string, status: string}) {
-    this.accounts.push(newAccount);
+    this.accountService.accounts.push(newAccount);
+    this.accounts = this.accountService.accounts;
+    console.log(this.accounts)
   }
 
   onStatusChanged(updateInfo: {id: number, newStatus: string}) {
-    this.accounts[updateInfo.id].status = updateInfo.newStatus;
+    this.accountService.accounts[updateInfo.id].status = updateInfo.newStatus;
+    this.accounts = this.accountService.accounts;
+  }
+
+  ngOnInit() {
+    this.accounts = this.accountService.accounts;
+  }
+
+  ngOnChanges() {
+    this.accounts = this.accountService.accounts;
   }
 }
