1e59057dc0a4ae9e1104fc0e1b3fdf327e871c62
austhomi <austhomi.k@hotmail.com>
2023-06-01 13:47:23 +0800

    Refactored the dropdown logic using HostBinding decorator

--- shopping-cart/src/app/app.component.ts
+++ shopping-cart/src/app/app.component.ts
@@ -1,15 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'shopping-cart';
   loadedFeature = 'recipe'
   
+  showHelloWorld = false;
+
   onFeatureSelected(feature: string){
     this.loadedFeature = feature;
+  }
+  ngOnInit(): void {
+    setInterval(() => {this.showHelloWorld = !this.showHelloWorld}, 1000)
   }
 }
