bab3353ecf5d8af504423dd7ef2615cd4c481504
austhomi <austhomi.k@hotmail.com>
2023-06-06 10:24:13 +0800

    Change to use Ingredient model as value type

--- shopping-cart/src/app/recipes/recipe.model.ts
+++ shopping-cart/src/app/recipes/recipe.model.ts
@@ -1,10 +1,12 @@
+import { Ingredient } from '../shared/ingredient.model';
+
 export class Recipe {
     public name: string;
     public description: string;
     public imagePath: string;
     public ingredients: {name: string, amount: number}[];
 
-    constructor(name: string, desc: string, imagePath: string, ingredients: {name: string, amount: number}[]){
+    constructor(name: string, desc: string, imagePath: string, ingredients: Ingredient[]){
         this.name = name;
         this.description = desc;
         this.imagePath = imagePath;
