09e9e145bda517a262ea34be10ccda1114516d45
austhomi <austhomi.k@hotmail.com>
2023-06-09 14:50:48 +0800

    Built custom observable

--- obs-04-operators/src/app/app.component.ts
+++ obs-04-operators/src/app/app.component.ts
@@ -1,12 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { UserService } from './user.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  constructor() {}
+export class AppComponent implements OnInit,OnDestroy {
+  userActivated = false;
+  private kepochi: Subscription; 
 
-  ngOnInit() {}
+  constructor(private userService:UserService) {}
+
+
+  ngOnInit() {
+    this.kepochi = this.userService.activatedEmitter.subscribe((activated) => {
+      this.userActivated = activated
+    })
+  }
+
+  ngOnDestroy(): void {
+      
+    this.kepochi.unsubscribe()
+  }
 }
--- obs-04-operators/src/app/user/user.component.ts
+++ obs-04-operators/src/app/user/user.component.ts
@@ -1,20 +1,30 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Params } from "@angular/router";
+import { Observer } from "rxjs";
+import { UserService } from "../user.service";
 
 @Component({
-  selector: 'app-user',
-  templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+  selector: "app-user",
+  templateUrl: "./user.component.html",
+  styleUrls: ["./user.component.css"],
 })
 export class UserComponent implements OnInit {
   id: number;
+  isActivated: boolean = false;
+  activatedObserver: Observer<Boolean>;
 
-  constructor(private route: ActivatedRoute) {
-  }
+  constructor(
+    private route: ActivatedRoute,
+    private userService: UserService
+  ) {}
 
   ngOnInit() {
     this.route.params.subscribe((params: Params) => {
       this.id = +params.id;
     });
+  }
+
+  onActivate() {
+    this.userService.activatedEmitter.next(true);
   }
 }
