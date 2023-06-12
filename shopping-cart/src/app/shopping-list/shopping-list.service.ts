ebb6214c0734c8fe77a2cde5638a44b054394206
austhomi <austhomi.k@hotmail.com>
2023-06-12 10:50:22 +0800

    181. Improving the reactive services with observables

--- shopping-cart/src/app/shopping-list/shopping-list.service.ts
+++ shopping-cart/src/app/shopping-list/shopping-list.service.ts
@@ -1,9 +1,9 @@
-import { EventEmitter } from "@angular/core";
+import { Subject } from "rxjs";
 import { Ingredient } from "../shared/ingredient.model";
 
 export class ShoppingListService {
 
-    ingredientsChanged = new EventEmitter<Ingredient[]>();
+    ingredientsChanged = new Subject<Ingredient[]>();
 
     private ingredients: Ingredient[] = [
         new Ingredient('Apples', 5),
@@ -18,21 +18,21 @@ export class ShoppingListService {
 
     addIngredient(ingredient: Ingredient) {
         this.ingredients.push(ingredient);
-        this.ingredientsChanged.emit(this.getIngredients())
+        this.ingredientsChanged.next(this.getIngredients())
     }
     
     addIngredients(ingredient: Ingredient[]) {
         this.ingredients.push(...ingredient);
-        this.ingredientsChanged.emit(this.getIngredients())
+        this.ingredientsChanged.next(this.getIngredients())
     }
 
     updateIngredient(id: number, recipe: Ingredient) {
         this.ingredients[id] = recipe
-        this.ingredientsChanged.emit(this.getIngredients())
+        this.ingredientsChanged.next(this.getIngredients())
     }
 
     deleteRecipe(id: number) {
         this.ingredients.splice(id, 1)
-        this.ingredientsChanged.emit(this.getIngredients())
+        this.ingredientsChanged.next(this.getIngredients())
     }
 }
